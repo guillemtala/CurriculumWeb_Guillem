01397c483aa8a3ec094ffc3592b89ca682cf2deb
Guillem Talayero Carrasco <Guillem>
2024-07-01 18:09:06 +0200

    Primer commit

---
+++ progress.js
@@ -0,0 +1,308 @@
+var circleBar = new ProgressBar.Circle('#circle-container', {
+    color: 'red',
+    strokeWidth: 10,
+    trailWidth: 10,
+    trailColor: 'white',
+    easing: 'easeInOut',
+    text: {
+        value: '75%',
+        className: 'progress-text',
+        style: {
+            color: 'white',
+            position: 'absolute',
+            top: '45%',
+            left: '42%',
+            padding: 0,
+            margin: 0,
+            transform: null
+        }
+    }
+
+});
+circleBar.animate(0.75, {
+    duration: 1500
+
+});
+var circleBar2 = new ProgressBar.Circle('#circle-container2', {
+    color: '#30dbcd',
+    strokeWidth: 10,
+    trailWidth: 10,
+    trailColor: 'white',
+    easing: 'easeInOut',
+    text: {
+        value: '70%',
+        className: 'progress-text',
+        style: {
+            color: 'white',
+            position: 'absolute',
+            top: '45%',
+            left: '42%',
+            padding: 0,
+            margin: 0,
+            transform: null
+        }
+    }
+
+});
+circleBar2.animate(0.70, {
+    duration: 1500
+
+});
+var circleBar3 = new ProgressBar.Circle('#circle-container3', {
+    color: '#41c885',
+    strokeWidth: 10,
+    trailWidth: 10,
+    trailColor: 'white',
+    easing: 'easeInOut',
+    text: {
+        value: '80%',
+        className: 'progress-text',
+        style: {
+            color: 'white',
+            position: 'absolute',
+            top: '45%',
+            left: '42%',
+            padding: 0,
+            margin: 0,
+            transform: null
+        }
+    }
+
+});
+circleBar3.animate(0.80, {
+    duration: 1500
+
+});
+var circleBar4 = new ProgressBar.Circle('#circle-container4', {
+    color: '#41aac8',
+    strokeWidth: 10,
+    trailWidth: 10,
+    trailColor: 'white',
+    easing: 'easeInOut',
+    text: {
+        value: '75%',
+        className: 'progress-text',
+        style: {
+            color: 'white',
+            position: 'absolute',
+            top: '45%',
+            left: '42%',
+            padding: 0,
+            margin: 0,
+            transform: null
+        }
+    }
+
+});
+circleBar4.animate(0.75, {
+    duration: 1500
+
+});
+
+var circleBar5 = new ProgressBar.Circle('#circle-container5', {
+    color: 'yellow',
+    strokeWidth: 10,
+    trailWidth: 10,
+    trailColor: 'white',
+    easing: 'easeInOut',
+    text: {
+        value: '80%',
+        className: 'progress-text',
+        style: {
+            color: 'white',
+            position: 'absolute',
+            top: '45%',
+            left: '42%',
+            padding: 0,
+            margin: 0,
+            transform: null
+        }
+    }
+
+});
+circleBar5.animate(0.80, {
+    duration: 1500
+
+});
+
+var circleBar6 = new ProgressBar.Circle('#circle-container6', {
+    color: 'orange',
+    strokeWidth: 10,
+    trailWidth: 10,
+    trailColor: 'white',
+    easing: 'easeInOut',
+    text: {
+        value: '25%',
+        className: 'progress-text',
+        style: {
+            color: 'white',
+            position: 'absolute',
+            top: '45%',
+            left: '42%',
+            padding: 0,
+            margin: 0,
+            transform: null
+        }
+    }
+
+});
+circleBar6.animate(0.25, {
+    duration: 1500
+
+});
+
+var soft1 = new ProgressBar.Circle('#soft1', {
+    color: 'pink',
+    strokeWidth: 10,
+    trailWidth: 10,
+    trailColor: 'white',
+    easing: 'easeInOut',
+    text: {
+        value: '95%',
+        className: 'progress-text',
+        style: {
+            color: 'white',
+            position: 'absolute',
+            top: '45%',
+            left: '42%',
+            padding: 0,
+            margin: 0,
+            transform: null
+        }
+    }
+
+});
+soft1.animate(0.95, {
+    duration: 1500
+
+});
+
+var soft2 = new ProgressBar.Circle('#soft2', {
+    color: 'yellow',
+    strokeWidth: 10,
+    trailWidth: 10,
+    trailColor: 'white',
+    easing: 'easeInOut',
+    text: {
+        value: '85%',
+        className: 'progress-text',
+        style: {
+            color: 'white',
+            position: 'absolute',
+            top: '45%',
+            left: '42%',
+            padding: 0,
+            margin: 0,
+            transform: null
+        }
+    }
+
+});
+soft2.animate(0.85, {
+    duration: 1500
+
+});
+
+var soft3 = new ProgressBar.Circle('#soft3', {
+    color: 'red',
+    strokeWidth: 10,
+    trailWidth: 10,
+    trailColor: 'white',
+    easing: 'easeInOut',
+    text: {
+        value: '90%',
+        className: 'progress-text',
+        style: {
+            color: 'white',
+            position: 'absolute',
+            top: '45%',
+            left: '42%',
+            padding: 0,
+            margin: 0,
+            transform: null
+        }
+    }
+
+});
+soft3.animate(0.90, {
+    duration: 1500
+
+});
+
+var soft4 = new ProgressBar.Circle('#soft4', {
+    color: 'orange',
+    strokeWidth: 10,
+    trailWidth: 10,
+    trailColor: 'white',
+    easing: 'easeInOut',
+    text: {
+        value: '90%',
+        className: 'progress-text',
+        style: {
+            color: 'white',
+            position: 'absolute',
+            top: '45%',
+            left: '42%',
+            padding: 0,
+            margin: 0,
+            transform: null
+        }
+    }
+
+});
+soft4.animate(0.90, {
+    duration: 1500
+
+});
+
+var soft5 = new ProgressBar.Circle('#soft5', {
+    color: '#bb63d7',
+    strokeWidth: 10,
+    trailWidth: 10,
+    trailColor: 'white',
+    easing: 'easeInOut',
+    text: {
+        value: '100%',
+        className: 'progress-text',
+        style: {
+            color: 'white',
+            position: 'absolute',
+            top: '45%',
+            left: '42%',
+            padding: 0,
+            margin: 0,
+            transform: null
+        }
+    }
+
+});
+soft5.animate(1, {
+    duration: 1500
+
+});
+
+var soft6 = new ProgressBar.Circle('#soft6', {
+    color: 'green',
+    strokeWidth: 10,
+    trailWidth: 10,
+    trailColor: 'white',
+    easing: 'easeInOut',
+    text: {
+        value: '90%',
+        className: 'progress-text',
+        style: {
+            color: 'white',
+            position: 'absolute',
+            top: '45%',
+            left: '42%',
+            padding: 0,
+            margin: 0,
+            transform: null
+        }
+    }
+
+});
+soft6.animate(0.90, {
+    duration: 1500
+
+});
